b9c57aff987544a95f36835185909a6c68fb3f31
Zalan Blenessy <zalan.blenessy@volvocars.com>
2025-05-23 16:55:34 +0200

    Rename plugin to checks-zuul

--- src/main/java/com/avm99963/gerrit/plugins/checks/zuul/HttpModule.java
+++ src/main/java/com/avm99963/gerrit/plugins/checks/zuul/HttpModule.java
@@ -9,6 +9,6 @@ public class HttpModule extends ServletModule {
   @Override
   protected void configureServlets() {
     DynamicSet.bind(binder(), WebUiPlugin.class)
-        .toInstance(new JavaScriptPlugin("gerrit-zuul-checks-plugin.js"));
+        .toInstance(new JavaScriptPlugin("checks-zuul.js"));
   }
 }
